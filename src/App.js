c9004507c3c4ca9872f57f2914bd85f13b5cee3c
Enes ÇEB <enesceb@hotmail.com>
2022-09-12 13:30:56 +0300

    file structure and routes created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@mui/material"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components' 
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <Box sx={{backgroundColor: '#000'}}>
+    <Navbar/>
+    <Routes>
+        <Route path="/" exact element={<Feed/>} />
+        <Route path="/video/:id" exact element={<VideoDetail/>} />
+        <Route path="/video/:id" exact element={<ChannelDetail/>} />
+        <Route path="/search/:searchTerm" exact element={<SearchFeed/>} />
+    </Routes>
+    </Box>
+    </BrowserRouter>
+
+  )
 }
 
-export default App;
+export default App
