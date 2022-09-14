74a0e6a24d60ef74b3767fda4b717f6fceb8ef54
Enes ÇEB <enesceb@hotmail.com>
2022-09-14 22:58:42 +0300

    Fetched Channel Detail datas

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
     <Routes>
         <Route path="/" exact element={<Feed/>} />
         <Route path="/video/:id" exact element={<VideoDetail/>} />
-        <Route path="/video/:id" exact element={<ChannelDetail/>} />
+        <Route path="/channel/:id" exact element={<ChannelDetail/>} />
         <Route path="/search/:searchTerm" exact element={<SearchFeed/>} />
     </Routes>
     </Box>
--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { Box } from "@mui/material"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { APIService } from "../utils/APIService"
+import {ChannelCard , Videos} from "./"
+
+
+
 
 const ChannelDetail = () => {
+  const {id} = useParams();
+  const [channelDetail, setchannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+  console.log(channelDetail, videos)
+
+  useEffect(() => {
+  APIService(`channels?part="snippet&id=${id}`)
+    .then((data) => setchannelDetail(data?.items[0]))
+   APIService(`search?channelId=${id}&part=snippet&order=date`)
+   .then((data) => setVideos(data?.items))
+  }, [id])
+  
   return (
-    <div>ChannelDetail</div>
+    <Box sx={{flexDirection: {sx:"column", md:"row"}}}>
+
+ 
+    </Box>
   )
 }
 
